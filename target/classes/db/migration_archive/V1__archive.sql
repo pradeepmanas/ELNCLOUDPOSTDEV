fdf7bee2353acbad02783871d73b07b7ba533e4e
pradeepmanas <pradeep@agaramtech.com>
2021-05-27 10:59:34 +0530

    update

--- target/classes/db/migration_archive/V1__archive.sql
+++ target/classes/db/migration_archive/V1__archive.sql
@@ -1,3 +1,11 @@
+
+CREATE SEQUENCE hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 CREATE TABLE IF NOT EXISTS public.lscfrarchivehistory
 (
     archivecode integer NOT NULL,
