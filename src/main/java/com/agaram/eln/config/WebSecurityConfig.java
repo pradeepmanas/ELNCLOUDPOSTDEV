2b397613dac2b3b8e1d2a5494de58673891fc9e4
pradeepmanas <pradeep@agaramtech.com>
2021-07-16 12:01:24 +0530

    update

--- src/main/java/com/agaram/eln/config/WebSecurityConfig.java
+++ src/main/java/com/agaram/eln/config/WebSecurityConfig.java
@@ -105,6 +105,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements A
 		antMatchers("/Instrument/download/{param}/{fileid}").permitAll().
 		antMatchers("/Instrument/Sharedcloudattachment").permitAll().
 		antMatchers("/multitenant/Registertenant").permitAll().
+		antMatchers("/multitenant/Registertenantid").permitAll().
 		antMatchers("/multitenant/Validatetenant").permitAll().
 		antMatchers("/multitenant/checktenantid").permitAll().
 		antMatchers("/multitenant/tenantlogin").permitAll().
